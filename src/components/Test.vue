62776edcfbab4d41e6806de1cbd9e360006a029b
wjxchn <1822630963@qq.com>
2020-11-11 20:49:32 +0800

    v0.02:新增Test.vue

---
+++ src/components/Test.vue
@@ -0,0 +1,36 @@
+<template>
+  <div>
+      <input v-model="testfirst">
+      <input v-model="testsecond">
+      <button @click="testclick">测试</button>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+    data(){
+        return{
+            testfirst: '',
+            testsecond: ''
+        }
+    },
+    methods:{
+        testclick(){
+            axios({
+                method: 'post',
+                url: 'http://localhost:9000/test',
+                data: {'testfirst': this.testfirst, 'testsecond': this.testsecond}
+            }).then(response=>{
+                console.log(response);
+            }).catch(error=>{
+                console.log(error);
+            });
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
