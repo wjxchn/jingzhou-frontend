48a433870fc47993f4371d120031d690ba24dc18
QingYuAnWayne <mrzhangveryhans@163.com>
2020-12-26 17:08:50 +0800

    更新了高被引论文

--- src/components/NewHomePage.vue
+++ src/components/NewHomePage.vue
@@ -27,21 +27,37 @@
     </el-input>
     </el-col>
     <el-col :span="1">
-    <el-button type="primary" icon="el-icon-search">搜索</el-button>
+    <el-button type="primary" icon="el-icon-search" >搜索</el-button>
     </el-col>
     </el-row>
     </div>
-  <div>
   <div style="height:100px">
   </div>
-    <el-card class="box-card" style="width:30%">
+  <el-row :gutter="20">
+  <el-col :span="8" :offset="3">
+    <el-card class="box-card">
       <div slot="header" class="clearfix" style="text-align:center">
-        <span>一周学术热点</span>
+        <span style="font-size:20px"><i class="el-icon-sunny"></i> 本周学术热点</span>
       </div>
-      <div v-for="o in 4" :key="o" class="text item">
-        {{'列表内容 ' + o }}
+      <div v-for="(o,s) in hotpoints" :index="s" class="text item" style="font-size:18px">
+        {{(s+1)+'.' + o}}
       </div>
     </el-card>
+  </el-col>
+  <el-col :span="8" :offset="2">
+    <el-card class="box-card">
+      <div slot="header" class="clearfix" style="text-align:center">
+        <span style="font-size:20px"><i class="el-icon-thumb"></i> 高被引论文</span>
+      </div>
+      <div v-for="(o,s) in papers" :index="s" v-if="s<10" class="text item" style="font-size:18px">
+      <el-link v-bind:href="'/paper?paperid=' + o.id">
+        {{(s+1)+'.'+o.title}}
+      </el-link>
+      </div>
+    </el-card>
+  </el-col>
+  </el-row>
+  <div style="height:50px">
   </div>
   </el-main>
 </el-container>
@@ -64,12 +80,67 @@
                     }, {
                     value: '选项3',
                     label: '主题'
-                    }]
+                    }],
+                hotpoints:["wjxtql！！！！！","B站10万点击量达成！","B站10万点击量达成！","B站10万点击量达成！"
+                ,"B站10万点击量达成！","B站10万点击量达成！","B站10万点击量达成！","B站10万点击量达成！","B站10万点击量达成！","B站10万点击量达成！"],
+                page7: 0,
+                itemnumber: 7,
+                papers: [],
+                papers2:[],
+                value: '',
             }
             },
         beforeCreate() {
         document.querySelector('body').setAttribute('style','background-color:rgb(255,255,255)')
-        }
+        },
+
+        method:{
+        },
+      created:function(){
+       this.$axios.get('/api/trend/paper/citation/rank',
+          {
+            params: {
+              pagenum: 0
+            }
+          }
+        ).then((res) => {
+          this.papers= [];
+          console.log(res.data.data.paperlist)
+          for(var i = 0; i < res.data.data.paperlist.length;i++){
+            var paper={}
+            paper.title=res.data.data.paperlist[i].title
+
+            if(res.data.data.paperlist[i].authors.length>0)
+            {
+              var authors = res.data.data.paperlist[i].authors[0].name
+              for(var j = 1;j<res.data.data.paperlist[i].authors.length;j++){
+                authors = authors +", "+ res.data.data.paperlist[i].authors[j].name
+              }
+              paper.authors=authors
+            }
+            else{
+              paper.authors="暂无相关信息"
+            }
+            if(res.data.data.paperlist[i].publisher===null){
+              paper.publisher="暂无相关信息"
+            }
+            else{
+              paper.publisher = res.data.data.paperlist[i].publisher
+            }
+            paper.year=res.data.data.paperlist[i].year
+            paper.n_citation=res.data.data.paperlist[i].n_citation
+            if(res.data.data.paperlist[i].id!==null){
+              paper.id=res.data.data.paperlist[i].id
+            }
+            else{
+              paper.id=res.data.data.paperlist[i].paperid
+            }
+            this.papers[i]=paper
+          }
+        }).catch((failResponse) => {
+          console.log(failResponse)
+        });
+      }
     }
 
 </script>
